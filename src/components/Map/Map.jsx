7654ea189ff68fc71f447c9e7c747c6e0d081984
Jose <jodiaz198@yahoo.com>
2021-08-08 03:14:39 -0700

    Adding more

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,9 +4,27 @@ import { Paper, Typography, useMediaQuery} from '@material-ui/core';
 import LocationOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 import useStyles from './style'
+
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates = { lat: 0, lng : 0}
+    
     return(
-        <h1>Map</h1>
+        <div className = {classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: 'AIzaSyAAAI9a-3sT9qKCRru5zMx5bVAa2wK4wUc'}}    
+                defaultCenter={coordinates}
+                center= {coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
