f0d7f354fde34640de323804a0fbab07d8a2ea63
Jose <jodiaz198@yahoo.com>
2021-08-14 18:22:50 -0700

    completed project

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,28 +1,68 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery} from '@material-ui/core';
-import LocationOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 import useStyles from './style'
+import { Data } from '@react-google-maps/api';
+import mapStyles from './mapStyles';
 
-const Map = () => {
+const Map = ({setCoordinates, setBounds, coordinates, places, setChildClicked, weatherData}) => {
     const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width:600px)');
-    const coordinates = { lat: 0, lng : 0}
+    const isDesktop = useMediaQuery('(min-width:600px)');
+  
+
+  
     
     return(
         <div className = {classes.mapContainer}>
             <GoogleMapReact
-                bootstrapURLKeys={{key: ''}}    
+                bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}    
                 defaultCenter={coordinates}
                 center= {coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
-                options={''}
-                onChange={''}
-                onChildClick={''}
+                options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles}}
+                onChange={(e) => {
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng});
+                    setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+                }}
+                onChildClick={(child) => setChildClicked(child)}
+                // onChildClick={''}
             >
+                {places?.map((place, i) => (
+                    <div
+                        className={classes.markerContainer}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
+                        key={i}
+                        >
+                        {
+                            !isDesktop ? (
+                                <LocationOnOutlinedIcon color='primary' fontSize='large'/>
+                            ) : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.typography} variant='subtitle2' gutterBottom>
+                                        {place.name}
+                                    </Typography>
+                                    <img 
+                                        className={classes.pointer}
+                                        src={place.photo ? place.photo.images.large.url : 'https://dobianchi.files.wordpress.com/2021/02/italian-restaurants-italy-covid.jpg'}
+                                        alt={place.name}
 
+                                    />
+                                    <Rating size="small" value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )
+                        }
+                    </div>
+                ))}
+                {weatherData?.list?.map((data, i)=> (
+                    <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+                        <img src={ `https://openweathermap.org/img/w/${data.weather[0].icon}.png ` }/>
+
+                    </div>
+                ))}
             </GoogleMapReact>
         </div>
     );
